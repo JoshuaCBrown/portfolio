a6b8c89f6bab665947c9137c8dd777462df283b8
JoshuaCBrown <[200~129564288+JoshuaCBrown@users.noreply.github.com~>
2023-10-10 06:27:09 -0400

    first commit, first idea for design. it's cool I think?

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="page-container">
+        <div className="sky"></div>
+        <div className="ground">
+          <div className="heading-container">
+            <div className="text-container">
+              <h1 className="title-text">Josh Brown Dev</h1>
+            </div>
+            <div className="reflection-container">
+              <h1 className="reflection-text">Josh Brown Dev</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
