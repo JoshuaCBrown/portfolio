87d232a8775b9c564780a24aa0092e449d382f5c
JoshuaCBrown <[200~129564288+JoshuaCBrown@users.noreply.github.com~>
2023-10-30 23:21:39 -0400

    add a non-fix to try to get nested routes to work with exit animations. Do research to find out exit animations is with nested routes (without re-rendering parent components) may not be possible. Resolve to most likely write sloppy code as a workaround.

--- src/sections/about/About.jsx
+++ src/sections/about/About.jsx
@@ -5,68 +5,13 @@ import TransitAnim from "../../transit-routes/TransitAnim";
 import "../../style/About.css";
 import TextBlockShaper from "./TextBlockShaper";
 import MyComponent from "./Test";
+import AboutContent from "./AboutContent";
 const About = ({ themeStyle }) => {
-  const [btnClicked, setBtnClicked] = useState(false);
-
-  const clickHandler = (itemId) => {
-    console.log("btn clicked");
-    console.log(btnClicked);
-    setBtnClicked(true);
-  };
-
-  const spring = {
-    type: "spring",
-    stiffness: 700,
-    damping: 30,
-  };
-
-  const abtNavBtns = [
-    { id: "education-box", title: "Education", link: "education" },
-    { id: "experience-box", title: "Experience", link: "experience" },
-    { id: "skills-box", title: "Skills", link: "skills" },
-    { id: "achievements-box", title: "Achievements", link: "achievements" },
-    { id: "interests-box", title: "Interests", link: "interests" },
-  ];
 
   return (
     <>
       <TransitAnim>
-        <div className="about-page-container">
-          <div className="about-content-container">
-            {btnClicked ? (
-              <></>
-            ) : (
-              <div className="about-summary">
-                <TextBlockShaper />
-              </div>
-            )}
-            {/* <MyComponent /> */}
-            <div className="about-nav-container">
-              <div className="left-spacer"></div>
-              <nav>
-                <ul className="about-nav" data-btnClicked={btnClicked}>
-                  {abtNavBtns.map((item) => (
-                    <Link to={item.link}>
-                    <motion.li
-                      key={item.id}
-                      className="about-box"
-                      id={item.id}
-                      onClick={() => clickHandler(item.id)}
-                      layout
-                    >
-                      
-                        <h2>{item.title}</h2>
-                      
-                    </motion.li>
-                    </Link>
-                  ))}
-                </ul>
-              </nav>
-              <div className="right-spacer"></div>
-            </div>
-            <Outlet />
-          </div>
-        </div>
+        <AboutContent />
       </TransitAnim>
     </>
   );
