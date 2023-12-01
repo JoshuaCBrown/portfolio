5094258de6b7f7518b7446c69554b27ef4ee11b0
JoshuaCBrown <[200~129564288+JoshuaCBrown@users.noreply.github.com~>
2023-12-01 02:13:43 -0500

    change about page animation to display different sections to incorporate brand new layout idea past the home 'about' page

--- src/sections/about/About.jsx
+++ src/sections/about/About.jsx
@@ -24,6 +24,7 @@ import "../../style/About.css";
 const About = ({ themeStyle }) => {
   const [goClicked, setGoClicked] = useState(false);
   const [aboutSection, setAboutSection] = useState("");
+  const [displayShowBox, setDisplayShowBox] = useState(true);
 
   const lightSvgVariants = {
     initial: { pathLength: 0 },
@@ -40,6 +41,19 @@ const About = ({ themeStyle }) => {
   const goClickHandler = () => {
     setGoClicked(true);
     setAboutSection("skills");
+    setDisplayShowBox((prevState) => !prevState);
+  };
+
+  const showBox = () => {
+    return displayShowBox
+      ? { flex: 1, transition: { duration: 0.25, delay: 0.25 } }
+      : { flex: 0, transition: { duration: 0.25, delay: 0 } };
+  };
+
+  const tellBox = () => {
+    return displayShowBox
+      ? { flex: 0, transition: { duration: 0.25, delay: 0 } }
+      : { flex: 1, transition: { duration: 0.25, delay: 0.25 } };
   };
 
   return (
@@ -52,31 +66,25 @@ const About = ({ themeStyle }) => {
               <div className="about-circles-container" data-goOn={goClicked}>
                 <motion.div layout className="photo-container">
                   <div className="photo-shaper">
-                    {goClicked ? (
-                      <h2 className="about-sub-heading">{aboutSection}</h2>
-                    ) : (
-                      <img src={phreshPhoto} className="fresh-foto" />
-                    )}
+                    <img src={phreshPhoto} className="fresh-foto" />
                   </div>
                 </motion.div>
-                <div className="remaining-circles" data-goOn={goClicked}>
-                  <motion.div
-                    className="circle-design"
-                    id="circle-one"
-                    layout
-                  ></motion.div>
-                  <motion.div
-                    className="circle-design"
-                    id="circle-two"
-                    layout
-                  ></motion.div>
-                  <motion.div
-                    className="circle-design"
-                    id="circle-three"
-                    layout
-                  ></motion.div>
-                </div>
-                {{ goClicked } ? <div className="place-holder"></div> : <></>}
+
+                <motion.div
+                  className="circle-design"
+                  id="circle-one"
+                  layout
+                ></motion.div>
+                <motion.div
+                  className="circle-design"
+                  id="circle-two"
+                  layout
+                ></motion.div>
+                <motion.div
+                  className="circle-design"
+                  id="circle-three"
+                  layout
+                ></motion.div>
               </div>
             </div>
             <motion.div layout className="about-left-right">
@@ -92,98 +100,69 @@ const About = ({ themeStyle }) => {
               )}
             </motion.div>
           </div>
-          <motion.div layout className="about-expansion-container">
-            <div className="about-expansion-middle">
-              <div className="about-expansion-upper-middle"></div>
-              <div className="about-expansion-lower-middle">
-                {aboutSection === "skills" && (
-                  <>
-                    <Skills />
-                  </>
-                )}
-                {aboutSection === "experience" && (
-                  <>
-                    <Experience />
-                  </>
-                )}
-                {aboutSection === "education" && (
-                  <>
-                    <Education />
-                  </>
-                )}
-                {aboutSection === "everything" && (
-                  <>
-                    <EverythingAboutMe />
-                  </>
-                )}
-              </div>
-            </div>
-            <div className="about-expansion-right">
-              <div className="empty-space"></div>
-              <div className="about-expansion-upper-right">
-                {goClicked ? (
-                  <>
-                    <div className="emblem-outer-ww-container">
-                      <div className="emblem-inner-ww-container">
-                        <WoodworkingImg svgVariants={lightSvgVariants} />
-                      </div>
-                    </div>
-                    {/* <div className="emblem-outer-bench-container">
-                      <div className="emblem-inner-bench-container">
-                        <BenchyImg svgVariants={lightSvgVariants} />
-                      </div>
-                    </div>
-                    <div className="emblem-outer-keeys-container">
-                      <div className="emblem-inner-keeys-container">
-                        <KeysImg svgVariants={lightSvgVariants} />
-                      </div>
-                    </div> */}
-                  </>
-                ) : (
-                  <></>
-                )}
-              </div>
-              <div></div>
-            </div>
-          </motion.div>
-          <div className="about-left-dashes"></div>
-          <motion.div layout className="about-middle-container">
-            <div className="intro-top-container"></div>
-            <div className="intro-mid-container">
-              <p>I like problem solving and making things</p>
-              <button className="go-button" onClick={goClickHandler}>
-                <img src={directionArrow} className="go-arrow" />
-              </button>
-            </div>
 
-            <div className="intro-bottom-container"></div>
-          </motion.div>
-          <motion.div layout className="about-right-container">
-            <div className="about-right-top">
-              <div className="about-right-top-left"></div>
-            </div>
-            <div className="about-right-middle">
-              <div className="about-right-middle-left"></div>
-              <div className="about-right-middle-middle">
-                <div className="skyline-svg-container">
-                  <SkylineImg svgVariants={lightSvgVariants} />
-                  <SkylineImgBg svgVariants={lightSvgVariants} />
+          <div className="flex-show-n-tell">
+            <motion.div
+              className="flex-show"
+              layout
+              initial={{ flex: 1 }}
+              animate={showBox}
+            >
+              <div className="about-left-dashes"></div>
+              <motion.div layout className="about-middle-container">
+                <div className="intro-top-container"></div>
+                <div className="intro-mid-container">
+                  <p>I like problem solving and making things</p>
+                  <button className="go-button" onClick={goClickHandler}>
+                    <img src={directionArrow} className="go-arrow" />
+                  </button>
                 </div>
-              </div>
-            </div>
-            <div className="about-right-bottom">
-              <div className="about-right-bottom-left"></div>
-              <div className="about-right-bottom-middle">
-                I live in Atlanta, GA
-              </div>
-            </div>
-          </motion.div>
-          <motion.div
-            className="tilted-container"
-            layout="size"
-            initial={{ scale: 1 }}
-            animate={goClicked ? { scale: 0 } : { scale: 1 }}
-          ></motion.div>
+
+                <div className="intro-bottom-container"></div>
+              </motion.div>
+              <motion.div
+                layout
+                transition={{ duration: 2 }}
+                className="about-right-container"
+              >
+                <div className="about-right-top">
+                  <div className="about-right-top-left"></div>
+                </div>
+                <div className="about-right-middle">
+                  <div className="about-right-middle-left"></div>
+                  <div className="about-right-middle-middle">
+                    <div className="skyline-svg-container">
+                      <SkylineImg svgVariants={lightSvgVariants} />
+                      <SkylineImgBg svgVariants={lightSvgVariants} />
+                    </div>
+                  </div>
+                </div>
+                <div className="about-right-bottom">
+                  <div className="about-right-bottom-left"></div>
+                  <div className="about-right-bottom-middle">
+                    I live in Atlanta, GA
+                  </div>
+                </div>
+              </motion.div>
+              <motion.div
+                className="tilted-container"
+                layout="size"
+                style={{ scale: 1 }}
+                initial={{ scale: 1 }}
+                animate={goClicked ? { scale: 0 } : { scale: 1 }}
+              ></motion.div>
+            </motion.div>
+            <motion.div
+              className="flex-tell"
+              layout
+              animate={tellBox}
+            ></motion.div>
+            <motion.div
+              className="flex-cape"
+              layout
+              animate={{ flex: 0 }}
+            ></motion.div>
+          </div>
           {/* <Skills />
         <Education /> 
         {themeStyle ? (
@@ -206,3 +185,64 @@ const About = ({ themeStyle }) => {
 };
 
 export default About;
+
+{
+  /* <motion.div
+  layout
+  transition={{ duration: 2, delay: 2 }}
+  className="about-expansion-container"
+>
+  <div className="about-expansion-middle">
+    <div className="about-expansion-upper-middle"></div>
+    <div className="about-expansion-lower-middle">
+      {aboutSection === "skills" && (
+                  <>
+                    <Skills />
+                  </>
+                )}
+                {aboutSection === "experience" && (
+                  <>
+                    <Experience />
+                  </>
+                )}
+                {aboutSection === "education" && (
+                  <>
+                    <Education />
+                  </>
+                )}
+                {aboutSection === "everything" && (
+                  <>
+                    <EverythingAboutMe />
+                  </>
+                )} 
+    </div>
+  </div>
+  <div className="about-expansion-right">
+    <div className="empty-space"></div>
+    <div className="about-expansion-upper-right">
+      {goClicked ? (
+        <>
+          <div className="emblem-outer-ww-container">
+            <div className="emblem-inner-ww-container">
+              <WoodworkingImg svgVariants={lightSvgVariants} />
+            </div>
+          </div>
+          <div className="emblem-outer-bench-container">
+                      <div className="emblem-inner-bench-container">
+                        <BenchyImg svgVariants={lightSvgVariants} />
+                      </div>
+                    </div>
+                    <div className="emblem-outer-keeys-container">
+                      <div className="emblem-inner-keeys-container">
+                        <KeysImg svgVariants={lightSvgVariants} />
+                      </div>
+                    </div> 
+        </>
+      ) : (
+        <></>
+      )}
+    </div>
+    <div></div>
+  </div>
+</motion.div>; */
+}
--- src/sections/about/AboutPosterity.jsx
+++ src/sections/about/AboutPosterity.jsx
@@ -1,11 +1,5 @@
-import { useState, useEffect } from "react";
-import { motion, AnimatePresence } from "framer-motion";
-import { Link, Outlet } from "react-router-dom";
-import { HashLink } from "react-router-hash-link";
-
-import TransitAnim from "../../transit-routes/TransitAnim";
-
-import TextBlockShaper from "./TextBlockShaper";
+import { useState } from "react";
+import { motion } from "framer-motion";
 
 import Education from "./Education.jsx";
 import Experience from "./Experience.jsx";
@@ -13,113 +7,202 @@ import Skills from "./Skills.jsx";
 import Achievements from "./Achievements.jsx";
 import Interests from "./Interests.jsx";
 
+import KeysImg from "../../assets/svgs/KeysImg.jsx";
+import WoodworkingImg from "../../assets/svgs/WoodworkingImg.jsx";
+import SkylineImg from "../../assets/svgs/SkylineImg.jsx";
+import SkylineImgBg from "../../assets/svgs/SkylineImgBg.jsx";
+import BenchyImg from "../../assets/svgs/BenchyImg.jsx";
+import phreshPhoto from "../../assets/about-photos/fresh-foto2.jpg";
+import directionArrow from "../../assets/commonicons/directional-arrow-two.png";
+
+import TransitAnim from "../../transit-routes/TransitAnim";
+
+import AboutHome from "./AboutHome";
+
 import "../../style/About.css";
 
 const About = ({ themeStyle }) => {
-  const [btnClicked, setBtnClicked] = useState(false);
-  const [catSelected, setCatSelected] = useState("");
+  const [goClicked, setGoClicked] = useState(false);
+  const [aboutSection, setAboutSection] = useState("");
 
-  const clickHandler = (itemId) => {
-    console.log("btn clicked");
-    console.log(btnClicked);
-    setBtnClicked(true);
-    setCatSelected(itemId);
+  const lightSvgVariants = {
+    initial: { pathLength: 0 },
+    animate: { pathLength: 1, transition: { duration: 2 } },
+    transition: { duration: 2 },
   };
 
-  const spring = {
-    type: "spring",
-    stiffness: 700,
-    damping: 30,
+  const darkSvgVariants = {
+    initial: { pathLength: 1 },
+    animate: { pathLength: 1, transition: { duration: 0 } },
+    transition: { duration: 0 },
   };
 
-  const abtNavBtns = [
-    { id: "education-box", title: "Education", link: "#education" },
-    { id: "experience-box", title: "Experience", link: "#experience" },
-    { id: "skills-box", title: "Skills", link: "#skills" },
-    {
-      id: "achievements-box",
-      title: "Achievements",
-      link: "#achievements",
-    },
-    { id: "interests-box", title: "Interests", link: "#interests" },
-  ];
+  const goClickHandler = () => {
+    setGoClicked(true);
+    setAboutSection("skills");
+  };
 
   return (
     <>
       <TransitAnim>
-        <div className="about-page-container">
-          <div className="about-content-container">
-            {btnClicked ? (
-              <></>
-            ) : (
-              <div className="about-summary">
-                <TextBlockShaper />
+        <div className="about-container" data-goOn={goClicked}>
+          <div className="about-left-container" data-goOn={goClicked}>
+            <div className="about-left-left"></div>
+            <div className="about-left-middle" data-goOn={goClicked}>
+              <div className="about-circles-container" data-goOn={goClicked}>
+                <motion.div layout className="photo-container">
+                  <div className="photo-shaper">
+                    {goClicked ? (
+                      <h2 className="about-sub-heading">{aboutSection}</h2>
+                    ) : (
+                      <img src={phreshPhoto} className="fresh-foto" />
+                    )}
+                  </div>
+                </motion.div>
+                <div className="remaining-circles" data-goOn={goClicked}>
+                  <motion.div
+                    className="circle-design"
+                    id="circle-one"
+                    layout
+                  ></motion.div>
+                  <motion.div
+                    className="circle-design"
+                    id="circle-two"
+                    layout
+                  ></motion.div>
+                  <motion.div
+                    className="circle-design"
+                    id="circle-three"
+                    layout
+                  ></motion.div>
+                </div>
+                {{ goClicked } ? <div className="place-holder"></div> : <></>}
               </div>
-            )}
-            {/* <MyComponent /> */}
-            <div className="about-nav-container">
-              <div className="left-spacer"></div>
-              <nav>
-                <ul className="about-nav" data-btnClicked={btnClicked}>
-                  {abtNavBtns.map((item) => (
-                    <motion.li
-                      key={item.id}
-                      className="about-box-li"
-                      
-                      onClick={() => clickHandler(item.id)}
-                      initial={false}
-                      layout
-                    >
-                      <HashLink smooth to={item.link}>
-                        <div className="about-box" id={item.id}>
-                          <h2>{item.title}</h2>
-                        </div>
-                      </HashLink>
-                    </motion.li>
-                  ))}
-                </ul>
-              </nav>
-              <div className="right-spacer"></div>
+            </div>
+            <motion.div layout className="about-left-right">
+              {goClicked ? (
+                <></>
+              ) : (
+                <>
+                  <div className="about-left-right-top">My name is Josh</div>
+                  <div className="about-left-right-bottom">
+                    <div className="about-left-right-bottom-top"></div>
+                  </div>
+                </>
+              )}
+            </motion.div>
+          </div>
+          <motion.div layout className="about-expansion-container">
+            <div className="about-expansion-middle">
+              <div className="about-expansion-upper-middle"></div>
+              <div className="about-expansion-lower-middle">
+                {aboutSection === "skills" && (
+                  <>
+                    <Skills />
+                  </>
+                )}
+                {aboutSection === "experience" && (
+                  <>
+                    <Experience />
+                  </>
+                )}
+                {aboutSection === "education" && (
+                  <>
+                    <Education />
+                  </>
+                )}
+                {aboutSection === "everything" && (
+                  <>
+                    <EverythingAboutMe />
+                  </>
+                )}
+              </div>
+            </div>
+            <div className="about-expansion-right">
+              <div className="empty-space"></div>
+              <div className="about-expansion-upper-right">
+                {goClicked ? (
+                  <>
+                    <div className="emblem-outer-ww-container">
+                      <div className="emblem-inner-ww-container">
+                        <WoodworkingImg svgVariants={lightSvgVariants} />
+                      </div>
+                    </div>
+                    {/* <div className="emblem-outer-bench-container">
+                      <div className="emblem-inner-bench-container">
+                        <BenchyImg svgVariants={lightSvgVariants} />
+                      </div>
+                    </div>
+                    <div className="emblem-outer-keeys-container">
+                      <div className="emblem-inner-keeys-container">
+                        <KeysImg svgVariants={lightSvgVariants} />
+                      </div>
+                    </div> */}
+                  </>
+                ) : (
+                  <></>
+                )}
+              </div>
+              <div></div>
+            </div>
+          </motion.div>
+          <div className="about-left-dashes"></div>
+          <motion.div layout className="about-middle-container">
+            <div className="intro-top-container"></div>
+            <div className="intro-mid-container">
+              <p>I like problem solving and making things</p>
+              <button className="go-button" onClick={goClickHandler}>
+                <img src={directionArrow} className="go-arrow" />
+              </button>
             </div>
 
-            {btnClicked ? (
-              <div className="about-cat-container">
-                <AnimatePresence>
-                  <motion.div
-                    className="about-cat"
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1, transition: { duration: 2 } }}
-                    transition={{ duration: 2 }}
-                  >
-                    <Education />
-                    <Experience />
-                    <Skills />
-                    <Achievements />
-                    <Interests />
-                  </motion.div>
-                </AnimatePresence>
+            <div className="intro-bottom-container"></div>
+          </motion.div>
+          <motion.div layout className="about-right-container">
+            <div className="about-right-top">
+              <div className="about-right-top-left"></div>
+            </div>
+            <div className="about-right-middle">
+              <div className="about-right-middle-left"></div>
+              <div className="about-right-middle-middle">
+                <div className="skyline-svg-container">
+                  <SkylineImg svgVariants={lightSvgVariants} />
+                  <SkylineImgBg svgVariants={lightSvgVariants} />
+                </div>
               </div>
-            ) : (
-              <></>
-            )}
+            </div>
+            <div className="about-right-bottom">
+              <div className="about-right-bottom-left"></div>
+              <div className="about-right-bottom-middle">
+                I live in Atlanta, GA
+              </div>
+            </div>
+          </motion.div>
+          <motion.div
+            className="tilted-container"
+            layout="size"
+            initial={{ scale: 1 }}
+            animate={goClicked ? { scale: 0 } : { scale: 1 }}
+          ></motion.div>
+          {/* <Skills />
+        <Education /> 
+        {themeStyle ? (
+          <>
+            <BenchyImg svgVariants={darkSvgVariants} />{" "}
+            <SkylineImg svgVariants={darkSvgVariants} />
+          </>
+        ) : (
+          <div style={{display: 'flex', width: '500px'}}>
+            <KeysImg svgVariants={lightSvgVariants} />
+            <WoodworkingImg svgVariants={lightSvgVariants} />
+            <BenchyImg svgVariants={lightSvgVariants} />
+            <SkylineImg svgVariants={lightSvgVariants} />
           </div>
+        )} */}
         </div>
       </TransitAnim>
     </>
   );
 };
+
 export default About;
-
-
-<div className="intro">My name is Josh</div>
-<div className="intro-two">
-  
-  I enjoy problem-solving and making things
-  <p>I live in Atlanta, and currently work in sales for a company that provides technology to largescale construction projects all over the world.</p>
-  <p>I spend a lot of my time creating. Some of my hobbies include</p>
-  <p>Becoming a programmer has been a lifelong goal of mine. While I am relatively new to coding, I have always had a strong passion for technology and computing. At the beginning of 2023, I was looking to make a career change from my previous role as an independent audio engineer and producer. While I enjoyed my work, as music has been a lifelong passion of mine, I was looking to get into something with more stability and room to grow. I was accepted into a program called Stiegler EdTech, which is similar to a coding bootcamp, except that it is free, and students are paid a stipend during the 6-month duration of the program and are guaranteed a job upon graduation. Unlike other profit-share bootcamps, graduates were not required to pay back the stipend, nor were they required to stay with their placement company for any period of time. The nature of this type of program made it highly competitive, however I performed well and was ecstatic to be accepted into the program. Unfortunately, due to the economic uncertainty unfolding during this time, the Atlanta cohort was delayed indefinitely. I was given the option to relocate to Detroit, Michigan to participate in their cohort but chose to remain close to my family and loved ones here in Atlanta.   
-    </p>
-    <p>
-      The application to apply to that program involved a multi-stage application and interview process, involving lengthy aptitude tests, logic puzzles, and critical thinking exercises desigend to test applicants on their future potential as a programmer. While I was undoubtedly disappointed the program did not work out, my acceptance into Stiegler gave me the assurance that this type of career could be something that I not only enjoyed, but excelled at. At that, I began learning to code in my freetime, using a mix of internet resources (namely The Odin Project and Free Code Camp) and several books. 
-    </p>
-</div>
